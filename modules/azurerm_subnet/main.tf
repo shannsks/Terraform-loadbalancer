703fcd2fe4f8ee24551b1bbf6394103edd5c6a81
shannsks <shannu.sks@gmail.com>
2025-09-07 11:54:32 -0400

    complete code sendign on github

---
+++ modules/azurerm_subnet/main.tf
@@ -0,0 +1,17 @@
+resource "azurerm_subnet" "subnet" {
+    name = var.subnet_name
+    resource_group_name = var.resource_group_name
+    virtual_network_name = var.virtual_network_name
+    address_prefixes = var.address_prefixes
+  
+}
+
+variable "subnet_name" {}
+
+variable "resource_group_name" {}
+variable "virtual_network_name" {
+  
+}
+variable "address_prefixes" {
+  
+}
---
+++ todoapp_infra/main.tf
@@ -0,0 +1,50 @@
+module "resource_grp" {
+  source                  = "../modules/Azurerm_resource_group"
+  resource_group_name     = "sks-ins"
+  resource_group_location = "central US"
+
+
+}
+
+module "virtual_network" {
+  depends_on               = [module.resource_grp]
+  source                   = "../modules/azurerm_virtual_network"
+  virtual_network_name     = "sksvnet"
+  virtual_network_location = "central US"
+  resource_group_name      = "sks-ins"
+  address_space            = ["10.0.0.0/16"]
+
+
+
+}
+
+module "subnet" {
+  depends_on           = [module.virtual_network]
+  subnet_name          = "sks-subnet"
+  source               = "../modules/azurerm_subnet"
+  resource_group_name  = "sks-ins"
+  virtual_network_name = "sks-vnet"
+  address_prefixes     = ["10.0.1.0/24"]
+
+}
+
+module "vir-machine" {
+  depends_on           = [module.resource_grp, module.subnet, module.virtual_network]
+  source               = "../modules/azurerm_virtual_machine"
+  nic_name             = "sks-nic"
+  location             = "central us"
+  resource_group_name  = "sks-ins"
+  vm_name              = "sks-vm"
+  vm_size              = "Standard_B1s"
+  admin_username       = "sksadmin"
+  admin_password       = "Welcome@2025"
+  image_publisher      = "Canonical"
+  image_offer          = "0001-com-ubuntu-server-focal"
+  image_sku            = "20_04_lts"
+  image_version        = "latest"
+  virtual_network_name = "sks-vnet"
+  frontend_subnet_name = "sks-datasubnet"
+
+
+
+}
